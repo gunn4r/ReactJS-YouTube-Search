debc7d2fd9c5d747db0f2b203c4dbe780946de26
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2016-07-17 16:25:15 -0600

    Up to github!

---
+++ src/components/video_detail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+  if(!video){
+    return <div>Loading...</div>;
+  }
+
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
+  return(
+    <div className="video-detail col-md-7">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className="details">
+        <div><h3>{video.snippet.title}</h3></div>
+        <div><p>{video.snippet.description}</p></div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
